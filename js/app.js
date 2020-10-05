819e436ab14662ec390cf1c90d9f365e9e239cfd
Noxde <70549780+Noxde@users.noreply.github.com>
2020-10-05 01:04:59 -0300

    "finished" mobile menu

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
 let burger = document.getElementById("burger");
 let mobileMenu = document.getElementById("mobile-menu");
+let overlay = document.getElementById("menu-overlay");
 
 function BurgerMenu() {
   if (burger.getAttribute("src") == "/images/icon-hamburger.svg") {
     burger.src = "/images/icon-close.svg";
     mobileMenu.style.display = "block";
+    overlay.style.display = "block";
   } else if (burger.getAttribute("src") == "/images/icon-close.svg") {
     burger.src = "/images/icon-hamburger.svg";
     mobileMenu.style.display = "none";
+    overlay.style.display = "none";
   }
 }
