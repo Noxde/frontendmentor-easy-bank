b83ab13b8db38b6af0ec2723047e476c7200c94c
Noxde <70549780+Noxde@users.noreply.github.com>
2020-10-04 22:42:47 -0300

    mobile

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 let burger = document.getElementById("burger");
+let mobileMenu = document.getElementById("mobile-menu");
 
 function BurgerMenu() {
   if (burger.getAttribute("src") == "/images/icon-hamburger.svg") {
     burger.src = "/images/icon-close.svg";
+    mobileMenu.style.display = "block";
   } else if (burger.getAttribute("src") == "/images/icon-close.svg") {
     burger.src = "/images/icon-hamburger.svg";
+    mobileMenu.style.display = "none";
   }
 }
