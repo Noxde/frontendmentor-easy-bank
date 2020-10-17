9ae9bd3662fdbba25456bd44a6ebf0279392cac9
Noxde <70549780+Noxde@users.noreply.github.com>
2020-10-17 03:53:20 -0300

    fix

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
-let burger = document.getElementById("burger");
+let burgerClosed = document.getElementById("burger-closed");
+let burgerOpened = document.getElementById("burger-opened");
 let mobileMenu = document.getElementById("mobile-menu");
 let overlay = document.getElementById("menu-overlay");
 
 function BurgerMenu() {
-  if (burger.getAttribute("src") == "/images/icon-hamburger.svg") {
-    burger.src = "/images/icon-close.svg";
-    mobileMenu.style.display = "block";
-    overlay.style.display = "block";
-  } else if (burger.getAttribute("src") == "/images/icon-close.svg") {
-    burger.src = "/images/icon-hamburger.svg";
+  if (burgerClosed.style.display == "inline-block") {
+    burgerOpened.style.display = "inline-block";
+    burgerClosed.style.display = "none";
+    mobileMenu.style.display = "inline-block";
+    overlay.style.display = "inline-block";
+  } else if (burgerOpened.style.display == "inline-block") {
+    burgerClosed.style.display = "inline-block";
+    burgerOpened.style.display = "none";
     mobileMenu.style.display = "none";
     overlay.style.display = "none";
   }
